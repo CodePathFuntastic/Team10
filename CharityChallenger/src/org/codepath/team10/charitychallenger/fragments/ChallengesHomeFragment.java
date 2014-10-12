9eb66cc5a60cb254b41b446a9ae3fc2447101816
Syed Naqvi <aftabnaqvi2003@hotmail.com>
2014-10-11 22:31:16 -0700

    Added ChallengesHomeFragment and its item
    Added few dummy images for charities.

---
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/fragments/ChallengesHomeFragment.java
@@ -0,0 +1,27 @@
+package org.codepath.team10.charitychallenger.fragments;
+
+import org.codepath.team10.charitychallenger.R;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChallengesHomeFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_home_challenges, container);
+		return view;
+	}
+
+}
