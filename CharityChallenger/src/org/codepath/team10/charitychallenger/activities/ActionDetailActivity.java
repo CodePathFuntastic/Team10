f15a13eee0f2adc617ebeea7862aaba921d26cc1
Kevin Mo <cassinomo@yahoo.com>
2014-10-11 22:50:09 -0700

    add ActionDetail screen

---
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/activities/ActionDetailActivity.java
@@ -0,0 +1,43 @@
+package org.codepath.team10.charitychallenger.activities;
+
+import org.codepath.team10.charitychallenger.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class ActionDetailActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_action_detail);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.action_detail, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void onTakePicture(View v) {
+		 Intent intent = new Intent(this, NewPictureActivity.class);
+		 startActivity(intent);
+	}
+}
