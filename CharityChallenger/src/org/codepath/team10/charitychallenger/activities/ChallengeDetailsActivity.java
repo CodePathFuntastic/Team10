0af23d31cb312e970c53fc05148b276eb817292b
Syed Naqvi <aftabnaqvi2003@hotmail.com>
2014-10-14 23:01:23 -0700

    Hooked up payment activity on Challenge details
    Also, enable back button navigation

--- CharityChallenger/src/org/codepath/team10/charitychallenger/activities/ChallengeDetailsActivity.java
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/activities/ChallengeDetailsActivity.java
@@ -5,6 +5,8 @@ import org.codepath.team10.charitychallenger.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ChallengeDetailsActivity extends Activity {
@@ -19,5 +21,26 @@ public class ChallengeDetailsActivity extends Activity {
 	public void onClickAccept(View v) {
 		 Intent intent = new Intent(this, ActionDetailActivity.class);
 		 startActivity(intent);
+	}
+	
+	public void onDonate(View view){
+		Intent intent = new Intent(this, DonateActivity.class);
+		startActivity(intent);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.new_picture, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == android.R.id.home) {
+			this.finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
