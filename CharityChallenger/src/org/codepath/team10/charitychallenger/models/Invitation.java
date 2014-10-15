85226239799feb56548cded29349b91f0981ce21
nsankaran <nsankaran@ebay.com>
2014-10-14 23:00:32 -0700

    adding models

---
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/models/Invitation.java
@@ -0,0 +1,38 @@
+package org.codepath.team10.charitychallenger.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName(value="Invitation")
+public class Invitation extends ParseObject {
+
+	/**
+	 * <ol>
+	 * 		<li>id:int</li>
+	 * 		<li>challengeId:int</li>
+	 * 		<li>senderId:id</li>
+	 * 		<li>receiver:id</li>
+	 * </ol>
+	 */
+	public Invitation(){
+	}
+	
+	public void setChallengeId( int id){
+		put("challengeId", id);
+	}
+	public int getChallengeId(){
+		return getInt("challengeId");
+	}
+	public void setSender(int user){
+		put("sender", user);
+	}
+	public int getSender(){
+		return getInt("sender");
+	}
+	public void setReceiver( int id){
+		put("receiver", id);
+	}
+	public int getReceiver(){
+		return getInt("receiver");
+	}
+}
--- CharityChallenger/src/org/codepath/team10/charitychallenger/models/Organization.java
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/models/Organization.java
@@ -1,5 +1,76 @@
 package org.codepath.team10.charitychallenger.models;
 
-public class Organization {
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
 
+@ParseClassName(value="Organization")
+public class Organization extends ParseObject{
+
+	/**
+	 * <ol>
+	 * 	<li>Name:String</li>
+	 *  <li>Description:String</li>
+	 *  <li>TargetAmount:int</li>
+	 *  <li>AmountRaised:int</li>
+	 *  <li>challengeId:int</li>
+	 *  <li>openChallenges:int</li>
+	 *  <li>completedChallenges:int</li>
+	 *  <li>paidChallenges:int</li>
+	 *  
+	 *  Should the list of photos be stored in this object?
+	 *  <li>Photos:List<String></li>
+	 * </ol>
+	 */
+	
+	public Organization(){
+	}
+	
+	public void setName(String name){
+		put("name", name);
+	}
+	public String getName(){
+		return getString("name");
+	}
+	public void setDescription(String desc){
+		put("description", desc);
+	}
+	public String getDescription(){
+		return getString("description");
+	}
+	public void setTargetAmount(double target){
+		put("target", target);
+	}
+	public double getTargetAmount(){
+		return getDouble("target");
+	}
+	public void setAmountRaised(double raised){
+		put("raised", raised);
+	}
+	public double getAmountRaised(){
+		return getDouble("raised");
+	}
+	public void setChallengeId( int id){
+		put("challenge_id", id);
+	}
+	public int getChallengeId(){
+		return getInt("challenge_id");
+	}
+	public void setOpenChallenges( int challenges){
+		put("open_challenges", challenges);
+	}
+	public int getOpenChallenges(){
+		return getInt("open_challenges");
+	}
+	public void setClosedChallenges(int challenges){
+		put("closed_challenges", challenges);
+	}
+	public int getClosedChallenges(){
+		return getInt("closed_challenges");
+	}
+	public void setPaidChallenges( int challenges){
+		put("paid_challenges", challenges);
+	}
+	public int getPaidChallenges(){
+		return getInt("paid_challenges");
+	}
 }
