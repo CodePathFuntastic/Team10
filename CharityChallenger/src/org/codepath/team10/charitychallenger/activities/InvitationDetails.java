8bb060f7bb819b059483c6895537cbb97d5923c0
nsankaran <nsankaran@ebay.com>
2014-10-16 17:36:56 -0700

    added invite friends activity

---
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/activities/InvitationDetails.java
@@ -0,0 +1,46 @@
+package org.codepath.team10.charitychallenger.activities;
+
+import org.codepath.team10.charitychallenger.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class InvitationDetails extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_invitation_details);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+		
+	public void onClickAccept(View v) {
+		 Intent intent = new Intent(this, NewPictureActivity.class);
+		 startActivity(intent);
+	}
+	
+	public void onDonate(View view){
+		Intent intent = new Intent(this, DonateActivity.class);
+		startActivity(intent);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.new_picture, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == android.R.id.home) {
+			this.finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/activities/InviteFriendsActivity.java
@@ -0,0 +1,15 @@
+package org.codepath.team10.charitychallenger.activities;
+
+import org.codepath.team10.charitychallenger.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InviteFriendsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_invite_friends);
+	}
+}
