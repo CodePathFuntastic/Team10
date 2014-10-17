d1260d98050204af1e7aeb3cb56d5e764ee36fbf
Kevin Mo <cassinomo@yahoo.com>
2014-10-17 13:07:19 -0700

    add friends list.

--- CharityChallenger/src/org/codepath/team10/charitychallenger/CharityChallengerApplication.java
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/CharityChallengerApplication.java
@@ -1,5 +1,7 @@
 package org.codepath.team10.charitychallenger;
 
+import java.util.Collection;
+
 import org.codepath.team10.charitychallenger.clients.TwitterRestClient;
 import org.codepath.team10.charitychallenger.models.Challenge;
 import org.codepath.team10.charitychallenger.models.Invitation;
@@ -7,22 +9,18 @@ import org.codepath.team10.charitychallenger.models.Organization;
 import org.codepath.team10.charitychallenger.models.Picture;
 import org.codepath.team10.charitychallenger.models.PictureUrl;
 import org.codepath.team10.charitychallenger.models.User;
-import org.codepath.team10.charitychallenger.parseuploads.ChallengeUploader;
-import org.codepath.team10.charitychallenger.parseuploads.InvitationUploader;
-import org.codepath.team10.charitychallenger.parseuploads.OrganizationUploader;
-import org.codepath.team10.charitychallenger.parseuploads.UserUploader;
 
 import android.app.Application;
 import android.content.Context;
 
 import com.activeandroid.ActiveAndroid;
+import com.facebook.model.GraphUser;
 import com.parse.Parse;
 import com.parse.ParseACL;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.ParseRole;
-import com.parse.ParseUser;
 
 public class CharityChallengerApplication extends Application {
 	
@@ -41,6 +39,16 @@ public class CharityChallengerApplication extends Application {
 		
 		//ParseTwitterUtils.initialize( Constants.TWITTER_CONSUMER_KEY, Constants.TWITTER_CONSUMER_SECRET);
 	}
+	
+    private Collection<GraphUser> selectedUsers;
+
+    public Collection<GraphUser> getSelectedUsers() {
+        return selectedUsers;
+    }
+
+    public void setSelectedUsers(Collection<GraphUser> selectedUsers) {
+        this.selectedUsers = selectedUsers;
+    }
 	
 	private void initializeFb() {
 		// Set your Facebook App Id in strings.xml
--- CharityChallenger/src/org/codepath/team10/charitychallenger/activities/InviteFriendsActivity.java
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/activities/InviteFriendsActivity.java
@@ -1,58 +1,121 @@
 package org.codepath.team10.charitychallenger.activities;
 
-import java.util.ArrayList;
-
+import org.codepath.team10.charitychallenger.CharityChallengerApplication;
 import org.codepath.team10.charitychallenger.R;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.widget.Toast;
 
-public class InviteFriendsActivity extends Activity {
+import com.facebook.FacebookException;
+import com.facebook.widget.FriendPickerFragment;
+import com.facebook.widget.PickerFragment;
+
+public class InviteFriendsActivity extends FragmentActivity {
+	
+	FriendPickerFragment friendPickerFragment;
+
+
+	public static void populateParameters(Intent intent, String userId, boolean multiSelect, boolean showTitleBar) {
+		intent.putExtra(FriendPickerFragment.USER_ID_BUNDLE_KEY, userId);
+		intent.putExtra(FriendPickerFragment.MULTI_SELECT_BUNDLE_KEY, multiSelect);
+		intent.putExtra(FriendPickerFragment.SHOW_TITLE_BAR_BUNDLE_KEY, showTitleBar);
+	}
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_invite_friends);
-		
-		ArrayList<String> friends = new ArrayList<String>();
-		
-		friends.add("Joe");
-		friends.add("Mary");
-		friends.add("Martha");
-		friends.add("Nathan");
-		friends.add("Syed");
-		friends.add("Kevin");
-		friends.add("Nambi");
-		friends.add("Shiva");
-		friends.add("Jim");
-		
-		ArrayAdapter<String> friendsAdapter = 
-			    new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friends);
-		
-		ListView list = (ListView)findViewById(R.id.lvListOfFriends);
-		list.setAdapter(friendsAdapter);
-		
-		//setOnItemClickListener
-		list.setOnItemClickListener( new OnItemClickListener(){
 
+		FragmentManager fm = getSupportFragmentManager();
+
+		if (savedInstanceState == null) {
+			final Bundle args = getIntent().getExtras();
+			friendPickerFragment = new FriendPickerFragment(args);
+			fm.beginTransaction()
+			.add(R.id.friend_picker_fragment, friendPickerFragment)
+			.commit();
+		} else {
+			friendPickerFragment = (FriendPickerFragment) fm.findFragmentById(R.id.friend_picker_fragment);
+		}
+
+		friendPickerFragment.setOnErrorListener(new PickerFragment.OnErrorListener() {
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				Intent intent = new Intent(InviteFriendsActivity.this, FunActivity.class);
-				startActivity(intent);
+			public void onError(PickerFragment<?> fragment, FacebookException error) {
+				InviteFriendsActivity.this.onError(error);
 			}
-			
+		});
+
+		friendPickerFragment.setOnDoneButtonClickedListener(new PickerFragment.OnDoneButtonClickedListener() {
+			@Override
+			public void onDoneButtonClicked(PickerFragment<?> fragment) {
+				// We just store our selection in the Application for other activities to look at.
+				CharityChallengerApplication application = (CharityChallengerApplication) getApplication();
+				application.setSelectedUsers(friendPickerFragment.getSelection());
+
+				setResult(RESULT_OK, null);
+				finish();
+			}
 		});
 	}
-	
-	public void onClickInvite( View view){
-		Intent intent = new Intent(this, FunActivity.class);
-		startActivity(intent);
+
+	private void onError(Exception error) {
+		String text = getString(R.string.exception, error.getMessage());
+		Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+		toast.show();
 	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		try {
+			// Load data, unless a query has already taken place.
+			friendPickerFragment.loadData(false);
+		} catch (Exception ex) {
+			onError(ex);
+		}
+	}
+//
+//	@Override
+//	protected void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_invite_friends);
+//		
+//		ArrayList<String> friends = new ArrayList<String>();
+//		
+//		friends.add("Joe");
+//		friends.add("Mary");
+//		friends.add("Martha");
+//		friends.add("Nathan");
+//		friends.add("Syed");
+//		friends.add("Kevin");
+//		friends.add("Nambi");
+//		friends.add("Shiva");
+//		friends.add("Jim");
+//		
+//		ArrayAdapter<String> friendsAdapter = 
+//			    new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friends);
+//		
+//		ListView list = (ListView)findViewById(R.id.lvListOfFriends);
+//		list.setAdapter(friendsAdapter);
+//		
+//		//setOnItemClickListener
+//		list.setOnItemClickListener( new OnItemClickListener(){
+//
+//			@Override
+//			public void onItemClick(AdapterView<?> parent, View view,
+//					int position, long id) {
+//				Intent intent = new Intent(InviteFriendsActivity.this, FunActivity.class);
+//				startActivity(intent);
+//			}
+//			
+//		});
+//	}
+//	
+//	public void onClickInvite( View view){
+//		Intent intent = new Intent(this, FunActivity.class);
+//		startActivity(intent);
+//	}
 }
