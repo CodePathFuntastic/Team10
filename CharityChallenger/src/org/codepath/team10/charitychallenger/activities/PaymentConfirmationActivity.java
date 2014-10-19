fc3b3e3a03f04b7eef28484989fcb84a3febe374
Kevin Mo <cassinomo@yahoo.com>
2014-10-18 17:14:07 -0700

    add paypal png and update the raised price.

--- CharityChallenger/src/org/codepath/team10/charitychallenger/activities/PaymentConfirmationActivity.java
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/activities/PaymentConfirmationActivity.java
@@ -3,11 +3,6 @@ package org.codepath.team10.charitychallenger.activities;
 import org.codepath.team10.charitychallenger.R;
 import org.codepath.team10.charitychallenger.helper.ParseProxyObject;
 
-import com.parse.GetCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +11,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 
 public class PaymentConfirmationActivity extends Activity {
 	
@@ -34,7 +35,7 @@ public class PaymentConfirmationActivity extends Activity {
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		
 		Intent intent = getIntent();
-		String donateAmount = intent.getStringExtra("amount");
+		final String donateAmount = intent.getStringExtra("amount");
 		ppo = (ParseProxyObject) intent.getSerializableExtra("parseObject");
 		Log.v("Test", String.format("Proxy object description: %s", ppo.getInt("orgId")));
 		
@@ -44,7 +45,7 @@ public class PaymentConfirmationActivity extends Activity {
 		tvDonationAmount = (TextView) findViewById(R.id.tvDonationAmount);
 		tvDonationAmount.setText(donateAmount);
 
-		int orgId = ppo.getInt("orgId");
+		final int orgId = ppo.getInt("orgId");
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("Organization");
 		query.whereEqualTo("org_id", orgId);
 		   query.getFirstInBackground(new GetCallback<ParseObject>() {
@@ -52,6 +53,22 @@ public class PaymentConfirmationActivity extends Activity {
 		            Log.d("Log","inside done :"+parseObject.getString("description"));
 		            if(ParseError == null){
 		            	updateView(parseObject);
+		            	int currentRaised = parseObject.getInt("raised");
+		            	int totalRaised = currentRaised + Integer.valueOf(donateAmount);
+		            	parseObject.put("raised", totalRaised);
+		            	parseObject.saveInBackground(new SaveCallback() {
+		                    public void done(ParseException e) {
+		                           if (e == null) {
+		                               Log.d("Log","EXCELENT");   
+
+		                           } else {
+
+		                               Log.d("Log","Failed boss: "+e);
+		                               System.out.println(e.getCause());
+		                               System.out.println("VERY BAD");     
+		                           }
+		                         }
+		                       });
 		            }else{
 		                 Log.d("Log", "Bombed error is :"+ParseError);
 		            }
@@ -60,7 +77,7 @@ public class PaymentConfirmationActivity extends Activity {
 		
 	}
 	
-	public void updateView(ParseObject parseObject) {
+	private void updateView(ParseObject parseObject) {
 		tvCharityNameOnConfirmation.setText(parseObject.getString("name"));
 		tvCharityConfirmationUrl.setText(parseObject.getString("url"));
 		tvCharityAddressOnConfirmation.setText(parseObject.getString("address"));
