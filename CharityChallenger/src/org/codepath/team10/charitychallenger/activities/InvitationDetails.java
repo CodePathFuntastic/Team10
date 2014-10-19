e5e8fcbc7a12166e34e1625ceee70af8522d7164
Syed Naqvi <aftabnaqvi2003@hotmail.com>
2014-10-18 19:30:26 -0700

    populated invitation details activity

--- CharityChallenger/src/org/codepath/team10/charitychallenger/activities/InvitationDetails.java
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/activities/InvitationDetails.java
@@ -1,21 +1,66 @@
 package org.codepath.team10.charitychallenger.activities;
 
+import java.util.List;
+
 import org.codepath.team10.charitychallenger.R;
+import org.codepath.team10.charitychallenger.helper.ParseProxyObject;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class InvitationDetails extends Activity {
+	private ParseObject mChallenge;
+	private TextView mTvChallengeName;
+	private TextView mTvTarget;
+	private TextView mTvRaised;
+	private TextView mTvDesc;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_invitation_details);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		int challengeId = 0;
+		Intent intent = getIntent();
+		if(intent.hasExtra("challengeId")){
+			challengeId = intent.getIntExtra("challengeId", 0); 
+		}
+		mTvChallengeName = (TextView) findViewById(R.id.tvCharityChallengeName);
+		mTvTarget = (TextView) findViewById(R.id.tvTargetAmountRaised);
+		mTvRaised = (TextView) findViewById(R.id.tvAmountRaised);
+		mTvDesc = (TextView) findViewById(R.id.tvDescription);
+		 
+		if(challengeId != 0){
+			ParseQuery<ParseObject> query = ParseQuery.getQuery("Challenge");
+			query.whereEqualTo("challenge_id", challengeId);
+			query.findInBackground(new FindCallback<ParseObject>() {
+	        public void done(List<ParseObject> records, ParseException e) {
+	        	if(e == null){
+	        		mTvChallengeName.setText(records.get(0).getString("name"));
+	        		int num = records.get(0).getInt("target");
+	        		mTvTarget.setText("$ "+num);
+	        		mTvRaised.setText("$ "+records.get(0).getInt("raised"));
+	        		mTvDesc.setText(""+records.get(0).getString("description"));
+	        	} else {
+	        		e.printStackTrace();
+	        	}
+	        }
+	        });
+		}
 	}
 		
 	public void onClickAccept(View v) {
