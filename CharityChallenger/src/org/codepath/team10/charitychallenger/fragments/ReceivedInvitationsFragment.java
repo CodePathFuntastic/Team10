10954e6c76a9443a8e757b08a3f579ee5b769ae9
Syed Naqvi <aftabnaqvi2003@hotmail.com>
2014-10-25 14:11:16 -0700

    Fixed conflicts.

--- CharityChallenger/src/org/codepath/team10/charitychallenger/fragments/ReceivedInvitationsFragment.java
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/fragments/ReceivedInvitationsFragment.java
@@ -1,15 +1,7 @@
 package org.codepath.team10.charitychallenger.fragments;
 
-<<<<<<< HEAD
-import java.util.List;
-
 import org.codepath.team10.charitychallenger.R;
 import org.codepath.team10.charitychallenger.listeners.InvitationsLoadedListener;
-import org.codepath.team10.charitychallenger.models.Invitation;
-=======
-import org.codepath.team10.charitychallenger.R;
-import org.codepath.team10.charitychallenger.listeners.InvitationsLoadedListener;
->>>>>>> FETCH_HEAD
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -17,14 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-<<<<<<< HEAD
-
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
-=======
->>>>>>> FETCH_HEAD
-
 
 public class ReceivedInvitationsFragment extends BaseInvitationsListFragment implements InvitationsLoadedListener{
 
@@ -37,13 +21,7 @@ public class ReceivedInvitationsFragment extends BaseInvitationsListFragment imp
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater,
-<<<<<<< HEAD
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-=======
-			@Nullable ViewGroup container, 
-			@Nullable Bundle savedInstanceState) {
-		
->>>>>>> FETCH_HEAD
 		super.onCreateView(inflater, container, savedInstanceState);
 		
 		eventManager.registerInvitationLoadedListener(this);
@@ -53,23 +31,12 @@ public class ReceivedInvitationsFragment extends BaseInvitationsListFragment imp
 		View view = inflater.inflate(R.layout.fragment_all_invitations, container, false); 
 		ListView lvInvitations = (ListView) view.findViewById(R.id.lvFragAllInvitations);
 		lvInvitations.setAdapter(invitationsAdapter);
-		
 		return view;
-<<<<<<< HEAD
-		
-		
-=======
->>>>>>> FETCH_HEAD
 	}
 
 	private void populateReceivedInvitations() {
 		if( parseData.getReceivedInvitations().size() == 0 ){
-			// fire a network call to load the sent invitations for the "user"
 			eventManager.getReceivedInvitations();
-<<<<<<< HEAD
-			
-=======
->>>>>>> FETCH_HEAD
 		}else{
 			invitations.addAll(parseData.getReceivedInvitations());
 		}
@@ -77,18 +44,11 @@ public class ReceivedInvitationsFragment extends BaseInvitationsListFragment imp
 
 	@Override
 	public void onSuccess() {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
 		invitations.addAll(parseData.getReceivedInvitations());
-=======
-		invitations.addAll( parseData.getSentInvitations());
->>>>>>> FETCH_HEAD
 		invitationsAdapter.notifyDataSetChanged();
 	}
 
 	@Override
 	public void onFailure(String message, Throwable error) {
-		// TODO Auto-generated method stub
-		
 	}
 }
