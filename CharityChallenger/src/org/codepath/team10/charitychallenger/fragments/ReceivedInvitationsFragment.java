226b54f79b68048feddbb327768b8faa00348efe
nsankaran <nsankaran@ebay.com>
2014-10-25 13:54:48 -0700

    done

--- CharityChallenger/src/org/codepath/team10/charitychallenger/fragments/ReceivedInvitationsFragment.java
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/fragments/ReceivedInvitationsFragment.java
@@ -1,24 +1,14 @@
 package org.codepath.team10.charitychallenger.fragments;
 
-import java.util.List;
-
 import org.codepath.team10.charitychallenger.R;
-import org.codepath.team10.charitychallenger.activities.BaseActivity;
 import org.codepath.team10.charitychallenger.listeners.InvitationsLoadedListener;
-import org.codepath.team10.charitychallenger.models.Invitation;
-import org.codepath.team10.charitychallenger.queries.InvitationQuery;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
 
 
 public class ReceivedInvitationsFragment extends BaseInvitationsListFragment implements InvitationsLoadedListener{
@@ -46,50 +36,20 @@ public class ReceivedInvitationsFragment extends BaseInvitationsListFragment imp
 		lvInvitations.setAdapter(invitationsAdapter);
 		
 		return view;
-
 	}
 
 	private void populateReceivedInvitations() {
 		if( parseData.getReceivedInvitations().size() == 0 ){
 			// fire a network call to load the sent invitations for the "user"
 			eventManager.getReceivedInvitations();
-//			String userId = parseData.getUser().getFacebookId();
-//			if( userId != null){
-//				
-//				ParseQuery<Invitation> query = ParseQuery.getQuery(Invitation.class);
-//				query.whereEqualTo("receiver", userId);
-//				
-//				try {
-//					List<Invitation> invites = query.find();
-//					if( invites != null ){
-//						addAllInvitations(invites);
-//					}
-//				} catch (ParseException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//				//query.clearCachedResult();
-//				query.findInBackground( new FindCallback<Invitation>(){
-//
-//					@Override
-//					public void done(List<Invitation> list,
-//										ParseException e) {
-//						if( e == null ){
-//							//parseData.getReceivedInvitations().addAll(list);
-//							addAllInvitations(list);
-//						}
-//					}
-//				});
-//			}
 		}else{
-			//addAllInvitations(parseData.getReceivedInvitations());
 			invitations.addAll(parseData.getReceivedInvitations());
 		}
 	}
 
 	@Override
 	public void onSuccess() {
-		invitations.addAll(parseData.getSentInvitations());
+		invitations.addAll( parseData.getSentInvitations());
 		invitationsAdapter.notifyDataSetChanged();
 	}
 
