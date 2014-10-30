b0a8943daea9d4f4551666af76233b644232e68b
nsankaran <nsankaran@ebay.com>
2014-10-29 21:02:02 -0700

    updated the challenges

--- CharityChallenger/src/org/codepath/team10/charitychallenger/parseuploads/ChallengeUploader.java
+++ CharityChallenger/src/org/codepath/team10/charitychallenger/parseuploads/ChallengeUploader.java
@@ -15,14 +15,14 @@ public class ChallengeUploader {
 		
 		c1.setChallengeId(100);
 		c1.setOrganization(100);
-		c1.setName("Eat a Cat");
-		c1.setDescription("Catch and Eat a cat");
+		c1.setName("Put whipped cream on your face");
+		c1.setDescription("Raise money for constructing new buildings for laurelwood school in sunnyvale, CA");
 		c1.setTargetAmount(1000);
 		c1.setAmountRaised(200);
 		c1.setOpenInvitation(200);
 		c1.setClosedInvitations(30);
 		c1.setPaidInvitations(40);
-		String[] pics1 = {"http://proof.nationalgeographic.com/files/2014/09/Sun-City-_Panorama.jpg", "http://proof.nationalgeographic.com/files/2014/03/MM8247_131003_0456941.jpg"};
+		String[] pics1 = {"http://www.t-g.com/photos/18/58/84/1858842-L.jpg", "http://cache3.asset-cache.net/gc/200017996-001-teenage-boy-having-cream-pie-thrown-at-gettyimages.jpg?v=1&c=IWSAsset&k=2&d=VR4Znm6PNEZbRnNhFRjSB1vp5BS7TLC5%2BPRnyrQXvQkbxxzEUrAL3EpI8vrNNd7a", "http://bloximages.chicago2.vip.townnews.com/nwitimes.com/content/tncms/assets/v3/editorial/b/cb/bcb91757-8307-51f0-9eb7-ef031465d9df/4f41a00748481.image.jpg"};
 		c1.addChallengePictureUrls(Arrays.asList(pics1));
 		c1.saveInBackground();
 		
@@ -30,14 +30,14 @@ public class ChallengeUploader {
 		Challenge c2 = new Challenge();
 		c2.setChallengeId(101);
 		c2.setOrganization(101);
-		c2.setName("Put your face in icecream");
-		c2.setDescription("Eat the icecream");
+		c2.setName("Jump into water");
+		c2.setDescription("Raise funds to purchase computers for low income kids. We have 300 kids.");
 		c2.setTargetAmount(5000);
 		c2.setAmountRaised(300);
 		c2.setOpenInvitation(100);
 		c2.setClosedInvitations(5);
 		c2.setPaidInvitations(30);
-		String[] pics2 = {"http://proof.nationalgeographic.com/files/2014/03/MM8247_131009_056653.jpg", "http://proof.nationalgeographic.com/files/2014/03/MM8247_131003_047027.jpg", "http://proof.nationalgeographic.com/files/2014/03/MM8247_131003_047346.jpg"};
+		String[] pics2 = {"http://www.thewizofodds.com/.a/6a00e553e551d18834011168eedaac970c-pi", "http://img.sparknotes.com/content/sparklife/sparktalk/jumpingdude_LargeWide.jpg", "https://c2.staticflickr.com/4/3649/3633471158_a87f1cc5e0.jpg"};
 		c2.addChallengePictureUrls(Arrays.asList(pics2));
 		c2.saveInBackground();
 
@@ -45,14 +45,14 @@ public class ChallengeUploader {
 		Challenge c3 = new Challenge();
 		c3.setChallengeId(102);
 		c3.setOrganization(102);
-		c3.setName("Put your face in icecream");
-		c3.setDescription("Eat the icecream");
+		c3.setName("Put egg on your face");
+		c3.setDescription("We are trying to protect the homeless people during winter by providing shelters during cold months. Please offer your support.");
 		c3.setTargetAmount(5000);
 		c3.setAmountRaised(300);
 		c3.setOpenInvitation(100);
 		c3.setClosedInvitations(5);
 		c3.setPaidInvitations(30);
-		String[] pics3 = {"http://proof.nationalgeographic.com/files/2014/06/140618-vorhes-turbines-03.jpg","http://proof.nationalgeographic.com/files/2014/06/140618-vorhes-turbines-01.jpg"};
+		String[] pics3 = {"http://www.stephenhannah.com/wp-content/uploads/egg-on-face.jpg", "http://bp2.blogger.com/_2upKmA8vnTk/SFghZC24-gI/AAAAAAAAALk/YbD4ENQisY0/s400/eggface.jpg", "http://itmakessenseblog.com/files/2012/04/Egg-on-face.jpg"};
 		c3.addChallengePictureUrls(Arrays.asList(pics3));
 		c3.saveInBackground();
 
@@ -60,14 +60,14 @@ public class ChallengeUploader {
 		Challenge c4 = new Challenge();
 		c4.setChallengeId(103);
 		c4.setOrganization(103);
-		c4.setName("Dress up like a bear");
-		c4.setDescription("Use a bug bear constume");
+		c4.setName("Dress up like a pirate");
+		c4.setDescription("Save the tropical forests of the world from loggers.");
 		c4.setTargetAmount(5000);
 		c4.setAmountRaised(300);
 		c4.setOpenInvitation(100);
 		c4.setClosedInvitations(5);
 		c4.setPaidInvitations(30);
-		String[] pics4 = {"http://proof.nationalgeographic.com/files/2014/10/20140923_PFWSS15_0589.jpg","http://proof.nationalgeographic.com/files/2014/10/landon-nordeman-pfw-2-18.nocrop.w1800.h1330.2x.jpg"};
+		String[] pics4 = {"http://media-cache-ec0.pinimg.com/236x/6f/f1/cb/6ff1cbcc279ed00c75fa4039f35e76ec.jpg", "http://www.dees-fancydress.co.uk/Pirates/sexy%20pirate.jpg","http://regmedia.co.uk/2010/04/30/pirates_westminster.jpg"};
 		c4.addChallengePictureUrls(Arrays.asList(pics4));
 		c4.saveInBackground();
 
@@ -75,14 +75,14 @@ public class ChallengeUploader {
 		Challenge c5 = new Challenge();
 		c5.setChallengeId(104);
 		c5.setOrganization(104);
-		c5.setName("Put your face on ICE for 2 minutes");
-		c5.setDescription("Get a big bar of ice from fridge");
+		c5.setName("Dress up like a baby");
+		c5.setDescription("We are trying to help underprevileged children aged less than 10 years by providing them care, shelter and education. ");
 		c5.setTargetAmount(3000);
 		c5.setAmountRaised(500);
 		c5.setOpenInvitation(500);
 		c5.setClosedInvitations(56);
 		c5.setPaidInvitations(76);
-		String[] pics5 = {"http://proof.nationalgeographic.com/files/2014/09/featured_image.jpg", "http://proof.nationalgeographic.com/files/2014/09/20140919-tracks-06.jpg"};
+		String[] pics5 = {"http://www.costumecollection.com.au/img/b/a/blue-adult-baby-funny-costume-f358dd69.jpg","http://f00.inventorspot.com/images/6-_Baby_Parents.jpg","http://www.essaytigers.com/images/adult-baby-boomer-costume.jpg"};
 		c5.addChallengePictureUrls(Arrays.asList(pics5));
 		c5.saveInBackground();
 
@@ -91,14 +91,14 @@ public class ChallengeUploader {
 		Challenge c6 = new Challenge();
 		c6.setChallengeId(105);
 		c6.setOrganization(105);
-		c6.setName("Put your face on ICE for 2 minutes");
-		c6.setDescription("Get a big bar of ice from fridge");
+		c6.setName("Carry your dog");
+		c6.setDescription("We rescue animals and offer them shelter");
 		c6.setTargetAmount(3000);
 		c6.setAmountRaised(500);
 		c6.setOpenInvitation(500);
 		c6.setClosedInvitations(56);
 		c6.setPaidInvitations(76);
-		String[] pics6 = {"http://images.nationalgeographic.com/wpf/media-live/photos/000/271/cache/autumn-colors_27164_600x450.jpg", "http://images.nationalgeographic.com/wpf/media-live/photos/000/271/cache/autumn-colors-white-mountains_27175_600x450.jpg"};
+		String[] pics6 = {"http://img2-1.timeinc.net/people/i/2011/pets/news/110926/dog-carrier-300.jpg","http://www.woohome.com/wp-content/uploads/2009/03/walkingthedog.jpg", "http://imghumour.com/assets/Uploads/Dog-Carry-Case.jpg", ""};
 		c6.addChallengePictureUrls(Arrays.asList(pics6));
 		c6.saveInBackground();
 		
